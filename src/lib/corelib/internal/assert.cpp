af18444689cb4735472758939098e17b3ed90fed
softboy <softboy@fhzc.com>
2015-10-27 20:16:41 +0800

    assert函数完成

--- src/lib/corelib/internal/assert.cpp
+++ src/lib/corelib/internal/assert.cpp
@@ -1,5 +1,6 @@
 #include "assert.h"
-
+#include "kernel/code_location.h"
+#include "kernel/errorinfo.h"
 #include <QString>
 
 namespace releasemgr 
@@ -14,7 +15,8 @@ void write_assert_location(const char *condition, const char *file, int line)
 
 void throw_assert_location(const char *condition, const char *file, int line)
 {
-   
+   throw ErrorInfo(QString::fromLatin1("ASSERT: %1").arg(QLatin1String(condition)),
+                   CodeLocation(QString::fromLocal8Bit(file), line, -1, false), true);
 }
 
 }//internal
