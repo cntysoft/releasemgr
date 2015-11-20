56441ea139c95f8e30e571f0f165a786f9c6283b
softboy <softboy@fhzc.com>
2015-11-20 17:25:37 +0800

    凤凰筑巢商家版默认配置文件完成

--- src/app/releasemgr/utils/default_cfg_initializer.cpp
+++ src/app/releasemgr/utils/default_cfg_initializer.cpp
@@ -11,11 +11,91 @@
 namespace releasemgr 
 {
 
+static void init_global_cfg(Settings &settings);
+static void init_fhzc_cfg(Settings &settings);
+static void init_fhshop_cfg(Settings& settings);
+
+void init_defualt_cfg(Settings& settings)
+{
+   init_global_cfg(settings);
+   init_fhzc_cfg(settings);
+   init_fhshop_cfg(settings);
+   settings.sync();
+}
+
 static void init_global_cfg(Settings& settings)
 {
    settings.setValue("db.host", "127.0.0.1", CFG_GROUP_GLOABL);
    settings.setValue("db.username", "root", CFG_GROUP_GLOABL);
    settings.setValue("db.charset", "utf8", CFG_GROUP_GLOABL);
+}
+
+static void init_fhshop_cfg(Settings& settings)
+{
+   //项目相关文件夹
+   settings.setValue("projectDir", "/srv/www/fenghuangshopdevel", CFG_GROUP_FHSHOP);
+   settings.setValue("buildDir", "/srv/www/fhshop-build", CFG_GROUP_FHSHOP);
+   settings.setValue("diffBuildDir", "/srv/www/fhshop-diff-build", CFG_GROUP_FHSHOP);
+   settings.setValue("docBuildDir", "/srv/www/fhshop-apidoc-build", CFG_GROUP_FHSHOP);
+   settings.setValue("senchaBuildDir", "PlatformJs/build/production", CFG_GROUP_FHSHOP);
+   //源码文件夹
+   QStringList sourceDirs{
+      "Apps",
+      "Data/Framework",
+      "Config",
+      "JsLibrary",
+      "Library",
+      "Modules",
+      "Statics",
+      "PlatformJs",
+      "SysApiHandler",
+      "TagLibrary",
+      "index.php"
+   };
+   settings.setValue("sourceDirs", sourceDirs, CFG_GROUP_FHSHOP);
+   
+   //用户组权限设置
+   settings.setValue("targetFsUserGroup.user", "cntysoft", CFG_GROUP_FHSHOP);
+   settings.setValue("targetFsUserGroup.group", "users", CFG_GROUP_FHSHOP);
+   settings.setValue("senchaCmdUserGroup.user", "cntysoft", CFG_GROUP_FHSHOP);
+   settings.setValue("senchaCmdUserGroup.group", "users", CFG_GROUP_FHSHOP);
+   
+   //设置sencha项目相关的
+   QStringList senchaProjectsFengHuang{
+      "PlatformJs/FengHuang/resources",
+      "PlatformJs/FengHuang/sass",
+      "PlatformJs/FengHuang/src",
+      "PlatformJs/FengHuang/app.js",
+      "PlatformJs/FengHuang/app.json",
+      "PlatformJs/FengHuang/bootstrap.css",
+      "PlatformJs/FengHuang/bootstrap.js",
+      "PlatformJs/FengHuang/index.html"
+   };
+   settings.setValue("senchaProjects.FengHuang", senchaProjectsFengHuang, CFG_GROUP_FHSHOP);
+   
+   //submodules 相关设置
+   QStringList submodulesPhp{
+      "Library/Cntysoft",
+      "Library/Zend",
+      "Library/Vender/VsImage"
+   };
+   QStringList submodulesSencha{
+      "PlatformJs/packages/cntysoft-comp",
+      "PlatformJs/packages/cntysoft-core",
+      "PlatformJs/packages/cntysoft-webos",
+      "PlatformJs/packages/sencha-ext"
+   };
+   QStringList submodulesJs{
+      "JsLibrary/CkEditor",
+      "JsLibrary/CodeMirror",
+      "JsLibrary/Jquery",
+      "JsLibrary/JsSha",
+      "JsLibrary/WebUploader"
+   };
+   settings.setValue("submodules.php", submodulesPhp, CFG_GROUP_FHSHOP);
+   settings.setValue("submodules.sencha", submodulesSencha, CFG_GROUP_FHSHOP);
+   settings.setValue("submodules.js", submodulesJs, CFG_GROUP_FHSHOP);
+   
 }
 
 static void init_fhzc_cfg(Settings& settings)
@@ -99,14 +179,5 @@ static void init_fhzc_cfg(Settings& settings)
    };
    settings.setValue("senchaProjects.FengHuang", senchaProjectsFengHuang, CFG_GROUP_FHZC);
 }
-
-void init_defualt_cfg(Settings& settings)
-{
-   init_global_cfg(settings);
-   init_fhzc_cfg(settings);
-   settings.sync();
-}
-
-
 
 }//releasemgr
