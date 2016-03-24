61abf92b374ff3afef23973477ef12bfe86c354c
SOFTBOY <softboy@fhzc.com>
2016-03-24 09:16:08 +0800

    修改筑巢aliyun常量

--- share/assets/zhuchao/distconst/NormalDeployment.php
+++ share/assets/zhuchao/distconst/NormalDeployment.php
@@ -8,6 +8,26 @@
  */
 namespace Cntysoft;
 /**
+ * @var string 商家系统的域名
+ */
+const RT_MERCHANT_DOMAIN = MERCHANT_DOMAIN;
+/**
+ * @var string 系统绑定的域名
+ */
+const RT_SYS_DOMAIN = SYS_DOMAIN;
+/**
+ * @var string 系统的主站域名
+ */
+const RT_SYS_SITE_NAME = SYS_SITE_NAME;
+/**
+ * @var string 采购商域名
+ */
+const RT_BUYER_SITE_NAME = BUYER_SITE_NAME;
+/**
+ * @var string 供应商域名
+ */
+const RT_PROVIDER_SITE_NAME = PROVIDER_SITE_NAME;
+/**
  * @var string
  */
 const RT_ALI_SEARCH_ENTRY = ALI_SEARCH_API_PUB_ENTRY;
@@ -20,13 +40,13 @@ const RT_OTS_API_ENTRY = OTS_PUB_API;
  */
 const RT_OSS_API_ENTRY = OSS_PUBLIC_ENTRY;
 /**
- * @var string 开放存储名称
+ * @var string 筑巢开放存储名称
  */
-const RT_CLOUD_CONTROLLER_OSS_IMG_BUCKET = CLOUD_CONTROLLER_OSS_IMG_BUCKET;
+const RT_ZHUCHAO_OSS_IMG_BUCKET = ZHUCHAO_OSS_IMG_BUCKET;
 /**
- * @var string 图片cdn地址
+ * @var string 筑巢图片cdn地址
  */
-const RT_CLOUD_CONTROLLER_IMG_CDN_SERVER = CLOUD_CONTROLLER_IMG_CDN_SERVER;
+const RT_ZHUCHAO_IMG_CDN_SERVER = ZHUCHAO_IMG_CDN_SERVER;
 /**
  * @var string 是否为开发者模式
  */
@@ -35,3 +55,7 @@ const RT_IS_DEVEL_MODEL = false;
  * @var string 部署类型
  */
 const RT_DEPLOY_TYPE = DEPLOY_TYPE_PUBLIC_PRODUCT;
+/**
+ * @var string 数据库名称
+ */
+const RT_ZHUCHAO_PLATFORM_DB = ZHUCHAO_PLATFORM_DBNAME;
